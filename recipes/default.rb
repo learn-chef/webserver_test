13de99a46a68e51a21db5c12f905bb70f612bdf6
tpetchel <tpetchel@gmail.com>
2016-05-17 10:05:54 -0400

    Code for RuboCop/Foodcritic lesson

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,22 +1,14 @@
-#
-# Cookbook Name:: webserver_test
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
 package_name = service_name = case node['platform']
 when 'centos' then 'httpd'
 when 'ubuntu' then 'apache2'
 end
 
-# Install the package.
 package package_name
 
-# Start and enable the service.
 service service_name do
   action [:enable, :start]
 end
 
-# Serve a custom home page.
 file '/var/www/html/index.html' do
   content '<html>
   <body>
